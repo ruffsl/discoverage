b2541ce09f6dcb528ac132d30d89482eddab5faa
Dominik Haumann <dhaumann@kde.org>
2012-12-11 17:16:10 +0100

    add config class

---
+++ config.cpp
@@ -0,0 +1,89 @@
+/* This file is part of the DisCoverage project.
+
+   Copyright (C) Dominik Haumann <dhaumann at rtr.tu-darmstadt.de>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "config.h"
+
+#include <QtCore/QSettings>
+
+Config* Config::s_self = 0;
+
+Config::Config()
+    : QObject()
+    , m_refCount(0)
+    , m_showVectorField(false)
+{
+    s_self = this;
+}
+
+Config::~Config()
+{
+    s_self = 0;
+}
+
+void Config::begin()
+{
+    +m_refCount;
+}
+
+void Config::end()
+{
+    --m_refCount;
+
+    if (m_refCount == 0) {
+        emit configChanged();
+    }
+}
+
+
+void Config::load(QSettings& config)
+{
+    begin();
+    config.beginGroup("config");
+
+    setShowVectorField(config.value("show-vector-field",  false).toBool());
+
+    config.endGroup();
+    end();
+}
+
+void Config::save(QSettings& config)
+{
+    config.beginGroup("config");
+
+    config.setValue("show-vector-field", showVectorField());
+
+    config.endGroup();
+}
+
+bool Config::showVectorField() const
+{
+    return m_showVectorField;
+}
+
+void Config::setShowVectorField(bool show)
+{
+    if (m_showVectorField == show)
+        return;
+
+    begin();
+    m_showVectorField = show;
+    end();
+}
+
+// kate: replace-tabs on; indent-width 4;
---
+++ config.h
@@ -0,0 +1,64 @@
+/* This file is part of the DisCoverage project.
+
+   Copyright (C) Dominik Haumann <dhaumann at rtr.tu-darmstadt.de>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef DISCOVERAGE_CONFIG_H
+#define DISCOVERAGE_CONFIG_H
+
+#include <QtCore/QObject>
+
+class QSettings;
+
+class Config : public QObject
+{
+    Q_OBJECT
+
+    static Config* s_self;
+
+    public:
+        Config(MainWindow* mainWindow, QWidget* parent = 0);
+        virtual ~Config();
+
+        static Config* self();
+
+        void load(QSettings& config);
+        void save(QSettings& config);
+
+    public slots:
+        void begin();
+        void end();
+
+    signals:
+        void configChanged();
+
+    //
+    // config setters and getters
+    //
+    public:
+        bool showVectorField() const;
+        void setShowVectorField(bool show);
+
+    private:
+        int m_refCount;
+
+        bool m_showVectorField;
+};
+
+#endif // DISCOVERAGE_CONFIG_H
+
+// kate: replace-tabs on; indent-width 4;
